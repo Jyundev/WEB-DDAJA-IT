a979e622f9dfc55c996d388c71b5f13f8c9486e0
Jyundev <jyunxxxxx@gmail.com>
2024-05-22 13:56:16 +0900

    Revert "CommonResponse 생성"
    This reverts commit a9604f3af360d29a25167b9eb7819022b1b7495c.

--- src/main/java/com/web/ddajait/config/constant/errorCode/Member.java
+++ src/main/java/com/web/ddajait/config/constant/errorCode/Member.java
@@ -1,4 +1,4 @@
-package com.web.ddajait.config.constant.Code;
+package com.web.ddajait.config.constant.errorCode;
 
 import org.springframework.http.HttpStatus;
 
@@ -12,13 +12,12 @@ import lombok.RequiredArgsConstructor;
  */
 @Getter
 @RequiredArgsConstructor
-public enum MemberError implements ErrorCode{
+public enum Member implements ErrorCode{
 
     MEMBER_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND,404,"존재하지 않는 사용자입니다"),
-    MEMBER_ID_ALREADY_EXISTS_ERROR(HttpStatus.CONFLICT,409, "중복된 사용자입니다."),
+    MEMBER_ID_ALREADY_EXISTS_ERROR(HttpStatus.CONFLICT,409, ""),
     MEMBER_EMAIL_ALREADY_EXISTS_ERROR(HttpStatus.CONFLICT,409,"는 이미 존재하는 이메일입니다."),
     INACTIVE_USER_ERROR(HttpStatus.FORBIDDEN, 403,"권한이 없는 사용자입니다"),
-    
     ;
     private final HttpStatus httpStatus;
     private final int code;
--- src/main/java/com/web/ddajait/controller/PublicApiController.java
+++ src/main/java/com/web/ddajait/controller/PublicApiController.java
@@ -1,20 +1,21 @@
 package com.web.ddajait.controller;
 
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.web.ddajait.model.dto.UserDto;
-import com.web.ddajait.model.dto.response.ResponseHandler;
 import com.web.ddajait.service.UserService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
@@ -28,27 +29,15 @@ public class PublicApiController {
     @Autowired 
     UserService userService;
 
-    // @Tag(name = "Public API")
-    // @Operation(summary = "회원가입", description = "회원가입 API 입니다. email, nickname, password 는 필수이며, age, gender는 선택사항입니다.")
-    // @PostMapping("/join")
-    // public void join(@Valid @RequestBody UserDto dto, HttpServletResponse response) throws Exception {
-    //     log.info("[PublicController][join] Start - Email: {}, Nickname: {}", dto.getEmail(), dto.getNickname());
-
-    //     userService.createMemberr(dto);
-    //     log.info("[PublicController][join] Success - User: {}", dto.getNickname());
-    //     response.sendRedirect("/loginPage"); // 리다이렉션 URL 설정
-
-    // }
-
     @Tag(name = "Public API")
     @Operation(summary = "회원가입", description = "회원가입 API 입니다. email, nickname, password 는 필수이며, age, gender는 선택사항입니다.")
     @PostMapping("/join")
-    public ResponseEntity join(@Valid @RequestBody UserDto dto) throws Exception {
+    public void join(@Valid @RequestBody UserDto dto, HttpServletResponse response) throws Exception {
         log.info("[PublicController][join] Start - Email: {}, Nickname: {}", dto.getEmail(), dto.getNickname());
+
         userService.createMemberr(dto);
         log.info("[PublicController][join] Success - User: {}", dto.getNickname());
-
-        return ResponseHandler.SUCCESS_SAMPLE(dto);
+        response.sendRedirect("/loginPage"); // 리다이렉션 URL 설정
 
     }
 }
