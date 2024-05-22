a9604f3af360d29a25167b9eb7819022b1b7495c
Jyundev <jyunxxxxx@gmail.com>
2024-05-22 12:27:21 +0900

    CommonResponse 생성

--- src/main/java/com/web/ddajait/config/constant/Code/CommonError.java
+++ src/main/java/com/web/ddajait/config/constant/Code/CommonError.java
@@ -1,4 +1,4 @@
-package com.web.ddajait.config.constant.errorCode;
+package com.web.ddajait.config.constant.Code;
 
 import org.springframework.http.HttpStatus;
 
@@ -15,12 +15,14 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public enum Common implements ErrorCode{
+public enum CommonError implements ErrorCode{
     INVALID_ARGUMENT_ERROR(HttpStatus.BAD_REQUEST, 400, "올바르지 않은 파라미터입니다."),
     INVALID_FORMAT_ERROR(HttpStatus.BAD_REQUEST,400, "올바르지 않은 포맷입니다."),
     INVALID_TYPE_ERROR(HttpStatus.BAD_REQUEST, 400, "올바르지 않은 타입입니다."),
     ILLEGAL_ARGUMENT_ERROR(HttpStatus.BAD_REQUEST, 400, "필수 파라미터가 없습니다"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 500, "서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요."),
+    DATA_ACEESS_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 500, "데이터에 접근 할 수 없습니다"),
+    OK(HttpStatus.OK, 200, "Ok")
     ;
     private final HttpStatus httpStatus;
     private final int code;
--- src/main/java/com/web/ddajait/config/constant/Code/MemberError.java
+++ src/main/java/com/web/ddajait/config/constant/Code/MemberError.java
@@ -1,4 +1,4 @@
-package com.web.ddajait.config.constant.errorCode;
+package com.web.ddajait.config.constant.Code;
 
 import org.springframework.http.HttpStatus;
 
@@ -12,12 +12,13 @@ import lombok.RequiredArgsConstructor;
  */
 @Getter
 @RequiredArgsConstructor
-public enum Member implements ErrorCode{
+public enum MemberError implements ErrorCode{
 
     MEMBER_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND,404,"존재하지 않는 사용자입니다"),
-    MEMBER_ID_ALREADY_EXISTS_ERROR(HttpStatus.CONFLICT,409, ""),
+    MEMBER_ID_ALREADY_EXISTS_ERROR(HttpStatus.CONFLICT,409, "중복된 사용자입니다."),
     MEMBER_EMAIL_ALREADY_EXISTS_ERROR(HttpStatus.CONFLICT,409,"는 이미 존재하는 이메일입니다."),
     INACTIVE_USER_ERROR(HttpStatus.FORBIDDEN, 403,"권한이 없는 사용자입니다"),
+    
     ;
     private final HttpStatus httpStatus;
     private final int code;
