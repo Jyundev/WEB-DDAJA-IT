0145581552d6302c90fe79ee1e71dad30fedba56
Jyundev <jyunxxxxx@gmail.com>
2024-05-23 09:31:25 +0900

    nickname size 설정

--- src/main/java/com/web/ddajait/model/dto/UserDto.java
+++ src/main/java/com/web/ddajait/model/dto/UserDto.java
@@ -8,6 +8,7 @@ import com.web.ddajait.config.constant.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -28,6 +29,7 @@ public class UserDto {
     private String email;
 
     @NotBlank
+    @Size(min = 2, max = 10, message = "닉네임은 최소 2자에서 최대 10자여야 합니다.")
     private String nickname;
 
     @NotBlank
