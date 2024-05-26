cdfdbf3f2d3134cef87a041fb3b455ea8f00c142
Jyundev <jyunxxxxx@gmail.com>
2024-05-26 16:13:09 +0900

    header api 추가

---
+++ src/main/java/com/web/ddajait/controller/HeaderController.java
@@ -0,0 +1,31 @@
+package com.web.ddajait.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.web.ddajait.config.handler.ResponseHandler;
+import com.web.ddajait.model.dto.ResponseDto;
+
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HeaderController {
+
+    @Operation(summary = "헤더 값 확인 ", description = "헤더 값 확인 API 입니다. 모든 요청들에 대해 컨트롤러 내부에서 Authorization 헤더 값을 그대로 추출합니다")
+    @GetMapping
+    public ResponseEntity<ResponseDto<Map<String, String>>> authHeaderChecker(HttpServletRequest request){
+        Map<String, String> response = new HashMap<>(){{
+            put("Authorization",  request.getHeader("Authorization"));
+        }};
+
+        return ResponseHandler.SUCCESS(response, "Header JWT Check");
+    }
+    
+}
