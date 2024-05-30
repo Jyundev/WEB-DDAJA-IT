45767362d1f81486354101ef495e6324e1945c3b
Jyundev <jyunxxxxx@gmail.com>
2024-05-30 15:42:38 +0900

    isLogin set

--- src/main/java/com/web/ddajait/service/CustomUserDetailsService.java
+++ src/main/java/com/web/ddajait/service/CustomUserDetailsService.java
@@ -14,30 +14,49 @@ import org.springframework.transaction.annotation.Transactional;
 import com.web.ddajait.model.entity.UserEntity;
 import com.web.ddajait.model.repository.UserRepository;
 
+import jakarta.servlet.ServletException;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@AllArgsConstructor
 @Component("userDetailsService")
 public class CustomUserDetailsService implements UserDetailsService {
    private final UserRepository userRepository;
-
-   public CustomUserDetailsService(UserRepository userRepository) {
-      this.userRepository = userRepository;
-   }
+   private final UserService userService;
 
    @Override
    @Transactional
    public UserDetails loadUserByUsername(final String email) {
+      log.info("[userDetailsService] [loadUserByUsername] Start");
+      
       return userRepository.findOneWithAuthoritiesByEmail(email)
             .map(user -> createUser(email, user))
             .orElseThrow(() -> new UsernameNotFoundException(email + " -> 데이터베이스에서 찾을 수 없습니다."));
    }
 
    private org.springframework.security.core.userdetails.User createUser(String email, UserEntity user) {
-      if (!user.getIsLogin()) {
-         throw new RuntimeException(email + " -> 활성화되어 있지 않습니다.");
+      // if (!user.getIsLogin()) {
+      //    throw new RuntimeException(email + " -> 활성화되어 있지 않습니다.");
+      // }
+
+
+      log.info("[userDetailsService] [createUser] user : "+user);
+
+      try {
+         userService.updateIsLoginByID(user.getEmail(), true);
+      } catch (ServletException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      } catch (Exception e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
       }
 
       List<GrantedAuthority> grantedAuthorities = user.getAuthorities().stream()
             .map(authority -> new SimpleGrantedAuthority(authority.getAuthorityName()))
             .collect(Collectors.toList());
+
 
       // Email이 로그인시 ID로 사용 됨
       return new org.springframework.security.core.userdetails.User(user.getEmail(),
