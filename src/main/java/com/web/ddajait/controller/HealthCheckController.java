a4e3b136f880ff20e17f55d7096b2a122ef0a8a7
Jyundev <jyunxxxxx@gmail.com>
2024-06-04 13:52:01 +0900

    설명 추가

--- src/main/java/com/web/ddajait/controller/HealthCheckController.java
+++ src/main/java/com/web/ddajait/controller/HealthCheckController.java
@@ -8,9 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @Service
 @RestController
 public class HealthCheckController {
+
+    /*
+     * 스프링부트 배포시 port 확인 용도
+     */
 
     @Value("${server.env}")
     private String env;
