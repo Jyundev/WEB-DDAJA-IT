375a3faa36f21dff6fe7ad989b4815324926e1b5
Jyundev <jyunxxxxx@gmail.com>
2024-06-14 16:28:51 +0900

    유저 오답 API

--- src/main/java/com/web/ddajait/controller/UserApiController.java
+++ src/main/java/com/web/ddajait/controller/UserApiController.java
@@ -132,27 +132,15 @@ public class UserApiController {
 
     }
 
-    @Operation(summary = "챌린지 상세 페이지 데이터", description = "챌린지 상세 페이지 데이터를 가져오는 API 입니다. \n*현제 데이터 오류로 challengeId = 7, 8, 9 테스트 가능")
+    @Operation(summary = "챌린지 상세 페이지 데이터", description = "챌린지 상세 페이지 데이터를 가져오는 API 입니다. \n*현제 데이터 오류로 challengeId = 1, 50 테스트 가능")
     @GetMapping("/challengePage/{challengeId}/{userId}")
-    public Challenge getchallengeDetailPageInfo(@PathVariable("challengeId") Long challengeId,
-            @PathVariable("userId") Long userId) throws Exception {
+    public Challenge getchallengeDetailPageInfo(
+            @PathVariable("userId") Long userId, @PathVariable("challengeId") Long challengeId) throws Exception {
 
         log.info("[ChallengeController][getchallengeDetailPageInfo] Starts");
 
-        return challengePartService.getChallengersDetailData(challengeId, userId);
+        return challengePartService.getChallengersDetailData(userId, challengeId);
     }
-
-    // @Operation(summary = "챌린지 데이별 메모 저장", description = "챌린지 데이별 메모를 저장하는 API.
-    // ChallengeId, UserId, Step, Day 값이 필요합니다")
-    // @PostMapping("/challengeMemo/{challengeId}/{userId}")
-    // public ResponseEntity<ResponseDto<MemoDto>>
-    // saveChallengeMemo(@PathVariable("challengeId") Long challengeId,
-    // @PathVariable("userId") Long userId,
-    // @Valid @RequestBody MemoDto dto) throws Exception {
-    // log.info("[ChallengeController][getchallengeDetailPageInfo] Starts");
-    // memoService.saveUserChallengeMemo(dto, userId, challengeId);
-    // return ResponseHandler.SUCCESS(dto, "챌린지 데이별 메모 저장 성공");
-    // }
 
     @Operation(summary = "챌린지 데이별 메모 업데이트", description = "챌린지 데이별 메모를 수정하는 API")
     @PostMapping("/challengeMemo/{challengeId}/{userId}")
@@ -176,21 +164,36 @@ public class UserApiController {
     @Operation(summary = "챌린지별 모든 기출문제 가져오기", description = "챌린지별 모든 파트의 기출문제를 제공합니다")
     @GetMapping("/challengePage/allQuestion/{certificateId}")
     public ResponseEntity<ResponseDto<List<PartQuestionDto>>> getAllPartQuestionByChallengeId(
-            @PathVariable("challengeId") Long certificateId)
+            @PathVariable("certificateId") Long certificateId)
             throws Exception {
         log.info("[ChallengeController][getAllPartQuestionByChallengeId] Starts");
-        List<PartQuestionDto> partQuestionDtos = partQuestionService.getQuestionListbyCertificatePartID(certificateId);
+        List<PartQuestionDto> partQuestionDtos = partQuestionService.getQuestionListbyCertificateId(certificateId);
         return ResponseHandler.SUCCESS(partQuestionDtos, "챌린지별 모든 기출문제 가져오기 성공");
     }
 
     @Operation(summary = "챌린지별 유저 오답문제 가져오기", description = "챌린지별 유저 오답문제를 제공합니다")
     @GetMapping("/challengePage/wrongQuestion/{challengeId}/{userId}")
-    public ResponseEntity<ResponseDto<List<UserWrongQuestionDto>>> getAllUserWrongQuestionById(
+    public ResponseEntity<ResponseDto<UserWrongQuestionDto>> getUserWrongQuestionById(
             @PathVariable("userId") Long userId, @PathVariable("challengeId") Long challengeId)
             throws Exception {
         log.info("[ChallengeController][getAllUserWrongQuestionById] Starts");
-        List<UserWrongQuestionDto> userWrongQuestionDtos = userWrongQuestionService.findWrongQuestionList(userId, challengeId);
-        return ResponseHandler.SUCCESS(userWrongQuestionDtos, "챌린지별 모든 기출문제 가져오기 성공");
+        UserWrongQuestionDto userWrongQuestionDtos = userWrongQuestionService.findWrongQuestionByUserIdChallengeId(
+                userId,
+                challengeId);
+        return ResponseHandler.SUCCESS(userWrongQuestionDtos, "챌린지별 유저 오답문제를 가져오기 성공");
+    }
+
+    @Operation(summary = "챌린지별 유저 오답문제 저장 및 수정", description = "챌린지별 유저 오답문제 저장 및 수정이 이루어집니다")
+    @PutMapping("/challengePage/wrongQuestion/{challengeId}/{userId}")
+    public ResponseEntity<ResponseDto<UserWrongQuestionDto>> modifyUserWrongQuestionByUserIdAndChalleneId(
+            @PathVariable("userId") Long userId, @PathVariable("challengeId") Long challengeId,
+            @RequestBody UserWrongQuestionDto uDto)
+            throws Exception {
+        log.info("[ChallengeController][modifyUserWrongQuestionByUserIdAndChalleneId] Starts");
+        userWrongQuestionService.modifyWrongQuestion(
+                userId,
+                challengeId, uDto);
+        return ResponseHandler.SUCCESS(uDto, "챌린지별 유저 오답문제를 저장하기 성공");
     }
 
     /* 유저 자격증 */
--- src/main/java/com/web/ddajait/service/impl/UserWrongQuestionServiceImpl.java
+++ src/main/java/com/web/ddajait/service/impl/UserWrongQuestionServiceImpl.java
@@ -1,19 +1,17 @@
 package com.web.ddajait.service.impl;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.web.ddajait.config.error.custom.WrongQuestionNotFoundException;
 import com.web.ddajait.model.dao.ChallengeInfoDao;
-import com.web.ddajait.model.dao.PartQuestionDao;
 import com.web.ddajait.model.dao.UserDao;
 import com.web.ddajait.model.dao.UserWrongQuestionDao;
 import com.web.ddajait.model.dto.UserWrongQuestionDto;
 import com.web.ddajait.model.entity.UserWrongQuestionEntity;
 import com.web.ddajait.service.UserWrongQuestionService;
+import com.web.ddajait.util.EntityUtil;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,29 +24,21 @@ public class UserWrongQuestionServiceImpl implements UserWrongQuestionService {
     private final UserWrongQuestionDao userWrongQuestionDao;
     private final UserDao userDao;
     private final ChallengeInfoDao challengeInfoDao;
-    private final PartQuestionDao partQuestionDao;
 
     @Override
-    public List<UserWrongQuestionDto> findWrongQuestionList(Long UserId, Long ChallengeId) throws Exception {
-        log.info("[UserWrongQuestionServiceImpl][findWrongQuestion] Starts");
-        Optional<List<UserWrongQuestionEntity>> wrongQuestions = userWrongQuestionDao.findWrongQuestionList(UserId,
-                ChallengeId);
-        return wrongQuestions.map(list -> list.stream()
-                .map(UserWrongQuestionDto::from)
-                .collect(Collectors.toList()))
-                .orElseGet(Collections::emptyList);
-
-    }
-
-    @Override
-    public void modifyWrongQuestion(UserWrongQuestionDto userWrongQuestionDto) throws Exception {
+    public void modifyWrongQuestion(Long userId, Long challengeId, UserWrongQuestionDto userWrongQuestionDto)
+            throws Exception {
         log.info("[UserWrongQuestionServiceImpl][modifyWrongQuestion] Starts");
 
         Optional<UserWrongQuestionEntity> wrongQuestionEntity = userWrongQuestionDao
-                .findWrongQuestionByQuestionId(userWrongQuestionDto.getQuestionId());
+                .findWrongQuestionByUserIdChallengeId(userId, challengeId);
         if (wrongQuestionEntity.isPresent()) {
             UserWrongQuestionEntity wEntity = wrongQuestionEntity.get();
+            EntityUtil.copyNonNullProperties(userWrongQuestionDto, wEntity);
+            wEntity.setUser(userDao.findById(userWrongQuestionDto.getUserId())
+                    .orElseThrow(() -> new RuntimeException("User not found")));
         } else {
+            saveWrongQuestion(userWrongQuestionDto);
         }
     }
 
@@ -56,25 +46,25 @@ public class UserWrongQuestionServiceImpl implements UserWrongQuestionService {
     public void saveWrongQuestion(UserWrongQuestionDto userWrongQuestionDto) throws Exception {
         log.info("[UserWrongQuestionServiceImpl][saveWrongQuestion] Starts");
         UserWrongQuestionEntity userWrongQuestionEntity = new UserWrongQuestionEntity();
+        EntityUtil.copyNonNullProperties(userWrongQuestionDto, userWrongQuestionEntity);
         userWrongQuestionEntity
-                .setChallengeInfo(challengeInfoDao.findById(userWrongQuestionDto.getChallengeId()).get());
-        userWrongQuestionEntity.setPartQuestion(partQuestionDao.findById(userWrongQuestionDto.getQuestionId()).get());
-        userWrongQuestionEntity.setUser(userDao.findById(userWrongQuestionDto.getUserId()).get());
-
+                .setChallengeInfo(challengeInfoDao.findById(userWrongQuestionDto.getChallengeId()).orElseThrow(() -> new RuntimeException("Challenge not found")));
+        userWrongQuestionEntity.setUser(userDao.findById(userWrongQuestionDto.getUserId()).orElseThrow(() -> new RuntimeException("User not found")));
+        ;
         userWrongQuestionDao.saveWrongQuestion(userWrongQuestionEntity);
-
     }
 
     @Override
-    public Optional<UserWrongQuestionDto> findWrongQuestionByQuestionId(Long questionId) throws Exception {
+    public UserWrongQuestionDto findWrongQuestionByUserIdChallengeId(Long UserId, Long ChallengeId) throws Exception {
+        log.info("[UserWrongQuestionServiceImpl][findWrongQuestionByUserIdChallengeId] Starts");
         Optional<UserWrongQuestionEntity> userWrongQuestionEntity = userWrongQuestionDao
-                .findWrongQuestionByQuestionId(questionId);
+                .findWrongQuestionByUserIdChallengeId(UserId, ChallengeId);
         if (userWrongQuestionEntity.isPresent()) {
-            UserWrongQuestionEntity userWrongEntity = userWrongQuestionEntity.get();
-
-            return Optional.of(UserWrongQuestionDto.from(userWrongEntity));
+            return UserWrongQuestionDto.from(userWrongQuestionEntity.get());
         } else {
-            return Optional.empty();
+
+            throw new WrongQuestionNotFoundException(
+                    "No wrong question found for userId: " + UserId + " and challengeId: " + ChallengeId);
         }
     }
 
