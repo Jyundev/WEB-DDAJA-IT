03d82cf3aca702b3b0ee17085eaa5028731018a9
Jyundev <jyunxxxxx@gmail.com>
2024-06-16 18:08:55 +0900

    로직 수정, cardto : from 추가 . repository findGetAll

--- src/main/java/com/web/ddajait/model/repository/ChallengeInfoRepository.java
+++ src/main/java/com/web/ddajait/model/repository/ChallengeInfoRepository.java
@@ -6,12 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.web.ddajait.model.entity.ChallengeInfoEntity;
+
 public interface ChallengeInfoRepository extends JpaRepository<ChallengeInfoEntity, Long> {
 
-    @Query(value = "SELECT * FROM challenge_info WHERE start_day  >= CURDATE() ORDER BY start_day LIMIT 10;",
-    nativeQuery = true)
+    @Query(value = "SELECT * FROM challenge_info WHERE start_day  >= CURDATE() ORDER BY start_day LIMIT 10;", nativeQuery = true)
     List<ChallengeInfoEntity> getRecentChallenges();
 
+    @Query(value = "SELECT * FROM challenge_info;", nativeQuery = true)
+    List<ChallengeInfoEntity> getAllChallenges();
 
-    
 }
